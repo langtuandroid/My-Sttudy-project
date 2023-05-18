fef4b6408a3f6d369d6ee741a13675bff1a4e371
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-05-19 04:33:33 +0600

    Update

--- Assets/Scripts/Reflectable Object Scripts/ReflectableObject.cs
+++ Assets/Scripts/Reflectable Object Scripts/ReflectableObject.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Interfaces;
 using UnityEngine;
 
@@ -5,6 +6,15 @@ namespace Reflectable_Object_Scripts
 {
     public class ReflectableObject : MonoBehaviour , IReflectable
     {
-        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Vector3 dir = (other.gameObject.transform.position - transform.position).normalized;
+                Vector3 punchOffset = dir + new Vector3(0.1f, 0.1f, 0.1f);
+                
+                transform.DOPunchPosition(punchOffset, 0.5f);
+            }
+        }
     }
 }
