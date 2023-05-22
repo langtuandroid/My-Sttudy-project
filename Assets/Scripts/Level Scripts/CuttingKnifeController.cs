9127cdb61c37a036018ba466b245aa6d72f473b9
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-05-22 19:55:14 +0600

    Update

---
+++ Assets/Scripts/Level Scripts/CuttingKnifeController.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Level_Scripts
+{
+    public class CuttingKnifeController : MonoBehaviour
+    {
+        [SerializeField] private Animator m_Animator;
+        private static readonly int Cutting = Animator.StringToHash("Cutting");
+
+        [Button]
+        private void KnifeCut()
+        {
+            m_Animator.SetTrigger(Cutting);
+        }
+    }
+}
