b9765cc03695ae9f1f7eee2fd1eb1e5cee6bccb2
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-05-22 22:03:01 +0600

    Updadte

---
+++ Assets/Scripts/Level Scripts/PringlesBottleActivator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Level_Scripts
+{
+    public class PringlesBottleActivator : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_PringlesBottle;
+        
+        
+    }
+}
