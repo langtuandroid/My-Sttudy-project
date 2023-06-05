35111ef4381520a2a8e23edbf2bf266ec64451d2
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-06-06 00:06:35 +0600

    Game Manager Singleton Added

---
+++ Assets/Scripts/Game Development Patterns with Unity 2021/Game Manager with (Singleton)/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game_Development_Patterns_with_Unity_2021.Game_Manager_with__Singleton_
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        private DateTime _sessionStartTime;
+        private DateTime _sessionEndTime;
+
+        private void Start()
+        {
+            // TODO:
+            // - Load player save
+            // - If no save, redirect player to registration scene
+            // - Call backend and get daily challenge and rewards 
+
+            _sessionStartTime = DateTime.Now;
+            Debug.Log("Game session start @: " + DateTime.Now);
+        }
+
+        void OnApplicationQuit()
+        {
+            _sessionEndTime = DateTime.Now;
+
+            TimeSpan timeDifference = _sessionEndTime.Subtract(_sessionStartTime);
+            
+            Debug.Log("Game session ended @: " + DateTime.Now);
+            Debug.Log("Game session lasted: " + timeDifference);
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Next Scene"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game Development Patterns with Unity 2021/Game Manager with (Singleton)/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Game_Development_Patterns_with_Unity_2021.Game_Manager_with__Singleton_
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance != null) return _instance;
+                    GameObject obj = new GameObject
+                    {
+                        name = nameof(T)
+                    };
+                    _instance = obj.AddComponent<T>();
+                }
+                return _instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
