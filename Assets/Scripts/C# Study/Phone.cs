6f2ff5f166ad628753a985d82f9e242c42a9d8aa
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-07-22 01:09:34 +0600

    Update

---
+++ Assets/Scripts/C# Study/Phone.cs
@@ -0,0 +1,9 @@
+﻿namespace C__Study
+{
+    public class Phone
+    {
+        public string brand;
+        public string model;
+        public int storege;
+    }
+}
---
+++ Assets/Scripts/C# Study/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace C__Study
+{
+    public class Program : MonoBehaviour
+    {
+        public event EventHandler<MyEventsArgs> OnSpacePress;
+        private int index;
+
+        public UnityEvent m_MyUnityEvent;
+        public class MyEventsArgs : EventArgs
+        {
+            public int Index;
+        }
+        
+        private void Start()
+        {
+            index = 0;
+            OnSpacePress += PressSpace;
+        }
+
+        private void PressSpace(object sender, MyEventsArgs e)
+        {
+            Debug.Log("Index : " + e.Index);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                index++;
+                OnSpacePress?.Invoke(this, new MyEventsArgs{Index = index});
+            }
+        }
+
+    }
+    
+}
