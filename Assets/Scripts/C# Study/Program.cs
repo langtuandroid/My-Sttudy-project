64ce7937df95d99920df34a468a2fc2c6aa267ef
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-07-24 08:52:59 +0600

    Update

--- Assets/Scripts/C# Study/Program.cs
+++ Assets/Scripts/C# Study/Program.cs
@@ -1,21 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace C__Study
 {
     public class Program : MonoBehaviour
     {
-        public delegate void TestDelegate();
-
-        private TestDelegate _testDelegateFunction;
-
-        private void Start()
+        [System.Serializable]
+        public struct Student
         {
-            _testDelegateFunction = delegate { Debug.Log("Anzamul Haque Akash 1"); };
-            _testDelegateFunction += delegate { Debug.Log("Anzamul Haque Akash 2"); };
-
-            _testDelegateFunction();
+            public int m_ID;
+            public string m_Name;
         }
 
+        public List<Student> m_Students;
+        
+        private void Start()
+        {
+            var sortedById = m_Students.OrderBy(s => s.m_ID);
+
+            foreach (var student in sortedById)
+            {
+                Debug.Log("Name : " + student.m_Name + "\n" + "ID : " + student.m_ID + "\n");
+            }
+        }
     }
     
 }
