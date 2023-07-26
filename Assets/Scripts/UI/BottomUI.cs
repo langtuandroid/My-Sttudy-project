e4687ad3f4f2e6c233d2626526b45872df2bc9ed
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-07-26 20:33:08 +0600

    Update

--- Assets/Scripts/UI/BottomUI.cs
+++ Assets/Scripts/UI/BottomUI.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -13,6 +14,27 @@ namespace UI
 
         private float _screenWidthChangeFlag;
 
+        private void Awake()
+        {
+            int buttonIndex = 0;
+            
+            foreach (OptionButton optionButton in m_OptionButtonList)
+            {
+                optionButton.GetComponent<Button>().onClick.AddListener(optionButton.Activate);
+                optionButton.m_Index = buttonIndex;
+
+                buttonIndex++;
+            }
+        }
+
+        private void Start()
+        {
+            foreach (OptionButton optionButton in m_OptionButtonList)
+            {
+                optionButton.OnButtonClick += ReSetUI;
+            }
+        }
+        
         private void Update()
         {
             float screenWidth = Screen.width;
@@ -21,6 +43,18 @@ namespace UI
                 _screenWidthChangeFlag = screenWidth;
                 GetResponsive(screenWidth);
             }
+        }
+        
+        private void ReSetUI(int activeButtonIndex)
+        {
+            for (int i = 0; i < m_OptionButtonList.Count; i++)
+            {
+                if(i == activeButtonIndex) continue;
+                m_OptionButtonList[i].Deactivate();
+            }
+            
+            float screenWidth = Screen.width;
+            GetResponsive(screenWidth);
         }
         
         private void GetResponsive(float screenWidth)
--- Assets/Scripts/UI/OptionButton.cs
+++ Assets/Scripts/UI/OptionButton.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 
 namespace UI
 {
     public class OptionButton : MonoBehaviour
     {
+        public event Action<int> OnButtonClick;
+        
         public bool m_IsActive;
+        public int m_Index;
+        public void Activate()
+        {
+            m_IsActive = true;
+            OnButtonClick?.Invoke(m_Index);
+        }
+
+        public void Deactivate() => m_IsActive = false;
     }
 }
