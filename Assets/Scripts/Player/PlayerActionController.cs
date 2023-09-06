80380748bdad427582dc27624a250c6a08917e66
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-09-07 01:34:04 +0600

    Update

---
+++ Assets/Scripts/Player/PlayerActionController.cs
@@ -0,0 +1,33 @@
+﻿using Sirenix.Utilities;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerActionController : MonoBehaviour
+    {
+        [SerializeField] private PlayerAnimationController m_PlayerAnimationController;
+
+
+        [SerializeField] private GameObject[] m_KatanaBox;
+        [SerializeField] private GameObject[] m_ActionKatanaBox;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                m_PlayerAnimationController.WorryIdleState();
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                m_PlayerAnimationController.Action1State();
+            }
+        }
+
+        public void ActionKatanaBoxActivator()
+        {
+            m_KatanaBox.ForEach(k => k.SetActive(false));
+            m_ActionKatanaBox.ForEach(k => k.SetActive(true));
+        }
+    }
+}
