f4c6d48306e650e850aad7063cdd39b9e5e51c7f
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-05 00:58:26 +0600

    Fps Update

---
+++ Assets/Scripts/FPS/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FPS.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private CharacterController m_CharacterController;
+        [SerializeField] private float m_Speed;
+        private void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Transform trans = transform;
+            Vector3 move = trans.right * x + trans.forward * z;
+
+            m_CharacterController.Move(move * (m_Speed * Time.deltaTime));
+        }
+    }
+}
