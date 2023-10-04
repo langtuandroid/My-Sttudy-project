add52e0c4db9701e0c5532b914d2658a3df16d72
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-05 01:42:48 +0600

    Update

--- Assets/Scripts/FPS/Player/PlayerMovement.cs
+++ Assets/Scripts/FPS/Player/PlayerMovement.cs
@@ -1,12 +1,37 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace FPS.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [Title("Player Movement Data")]
         [SerializeField] private CharacterController m_CharacterController;
         [SerializeField] private float m_Speed;
+        [SerializeField] private float m_Gravity = -9.81f;
+        [SerializeField] private float m_JumpHeight = 3f;
+        [SerializeField] private Transform m_GroundCheck;
+        [SerializeField] private float m_GroundCheckDistance = 0.4f;
+        [SerializeField] private LayerMask m_GroundCheckLayerMask;
+        
+        private Vector3 velocity;
+        private bool isGrounded;
+        
         private void Update()
+        {
+            GroundCheck();
+            Move();
+            if (Input.GetButtonDown("Jump") && isGrounded) Jump();
+            ApplyGravity();
+        }
+
+        private void GroundCheck()
+        {
+            isGrounded = Physics.CheckSphere(m_GroundCheck.position, m_GroundCheckDistance, m_GroundCheckLayerMask);
+            if (isGrounded && velocity.y < 0) velocity.y = -5f;
+        }
+
+        private void Move()
         {
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
@@ -15,6 +40,17 @@ namespace FPS.Player
             Vector3 move = trans.right * x + trans.forward * z;
 
             m_CharacterController.Move(move * (m_Speed * Time.deltaTime));
+        }
+
+        private void Jump()
+        {
+            velocity.y += Mathf.Sqrt(m_JumpHeight * -2 * m_Gravity);
+        }
+
+        private void ApplyGravity()
+        {
+            velocity.y += m_Gravity * Time.deltaTime;
+            m_CharacterController.Move(velocity * Time.deltaTime);
         }
     }
 }
