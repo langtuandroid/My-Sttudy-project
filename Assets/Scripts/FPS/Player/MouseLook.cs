9f962a68cb21f6a2754bb9e2e781848a013b66e2
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-05 00:44:46 +0600

    FPS Mouse Look Update

---
+++ Assets/Scripts/FPS/Player/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FPS.Player
+{
+    public class MouseLook : MonoBehaviour
+    {
+
+        [SerializeField] private Transform m_Player;
+        [SerializeField] private float m_MouseSensitivity = 100f;
+        [SerializeField, MinMaxSlider(-360f, 360f, true)] private Vector2 m_XClampRotation;
+
+        private float xRotation;
+
+        private void Start()
+        { 
+            xRotation = 90f;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") *  m_MouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") *  m_MouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, m_XClampRotation.x, m_XClampRotation.y);
+            
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            m_Player.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
