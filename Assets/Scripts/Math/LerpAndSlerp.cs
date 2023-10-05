ecb2170f1dd4c4db3167b26d9574e9d4bf5a6629
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-05 18:05:41 +0600

    math Update

--- Assets/Scripts/Math/LerpAndSlerp.cs
+++ Assets/Scripts/Math/LerpAndSlerp.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Math
@@ -6,25 +7,16 @@ namespace Math
     {
         [SerializeField] private Transform m_LerpPoint;
         [SerializeField] private Transform m_SlerpPoint;
-        [SerializeField] private Transform m_Point1;
-        [SerializeField] private Transform m_Point2;
+        [SerializeField] private Transform m_TargetPos;
 
         [SerializeField] private float m_MoveSpeed;
 
-        private void Start()
-        {
-            Vector3 intItPos = m_Point1.position;
-            m_LerpPoint.position = intItPos;
-            m_SlerpPoint.position = intItPos;
-        }
-
         private void Update()
         {
-            Vector3 targetPos = m_Point2.position;
+            Vector3 targetPos = m_TargetPos.position;
             
             Vector3 newPositionLerpPoint = Vector3.Lerp(m_LerpPoint.position, targetPos, m_MoveSpeed * Time.deltaTime);
             m_LerpPoint.position = newPositionLerpPoint;
-            
             
             Vector3 newPositionSlerpPoint = Vector3.Slerp(m_SlerpPoint.position, targetPos, m_MoveSpeed * Time.deltaTime);
             m_SlerpPoint.position = newPositionSlerpPoint;
@@ -32,15 +24,22 @@ namespace Math
 
         private void OnDrawGizmos()
         {
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = Color.white;
+            style.fontSize = 16;
+            style.alignment = TextAnchor.MiddleLeft - 1;
+            
             Gizmos.color = Color.red;
-            Gizmos.DrawSphere(m_LerpPoint.position, 0.05f);
+            Gizmos.DrawSphere(m_LerpPoint.position, 0.08f);
+            Handles.Label(m_LerpPoint.position, "Lerp", style);
 
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawSphere(m_SlerpPoint.position, 0.05f);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(m_SlerpPoint.position, 0.08f);
+            Handles.Label(m_SlerpPoint.position, "Slerp", style);
             
             Gizmos.color = Color.cyan;
-            Gizmos.DrawSphere(m_Point1.position, 0.1f);
-            Gizmos.DrawSphere(m_Point2.position, 0.1f);
+            Gizmos.DrawSphere(m_TargetPos.position, 0.1f);
+            Handles.Label(m_TargetPos.position, "Target", style);
         }
     }
 }
