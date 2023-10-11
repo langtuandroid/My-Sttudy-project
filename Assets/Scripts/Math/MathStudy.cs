44351e5256a998202609c7a473d86ebbc264f49b
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-12 01:46:52 +0600

    Update

---
+++ Assets/Scripts/Math/MathStudy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Math
+{
+    public class MathStudy : MonoBehaviour
+    {
+        [SerializeField] private float m_SignXInput;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                //-------------1D Vector Direction (Will be -1 or 1)---------------
+                if (m_SignXInput == 0f)
+                {
+                    Debug.Log("Sign Input can't be ZERO!!");
+                    return;
+                }
+                Debug.Log("Direction using Sign = " + Mathf.Sign(m_SignXInput));
+                //----------------------------------------------------------------
+            }
+        }
+    }
+}
