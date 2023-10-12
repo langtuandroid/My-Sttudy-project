48e52f43064e431eb2d3935ffeafd0bf9826162b
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-13 02:31:07 +0600

    Update

--- Assets/Scripts/Math/MathStudy.cs
+++ Assets/Scripts/Math/MathStudy.cs
@@ -23,5 +23,27 @@ namespace Math
         [Button]
         private void IDVectorTwoPointDistance() => Debug.Log("PointA and PointB Distance = " + Mathf.Abs(m_PointA - m_PointB));
         //------------------------------------------------------------------------------------------------------------------------------------
+
+        [SerializeField] private Vector2 m_A;
+        [SerializeField] private Vector2 m_B;
+        [SerializeField] private Vector2 m_APlusB;
+        private void OnDrawGizmos()
+        {
+            m_APlusB = m_A + m_B;
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(Vector2.zero, m_A);
+            Gizmos.DrawSphere(m_A, 0.1f);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(Vector2.zero, m_B);
+            Gizmos.DrawSphere(m_B, 0.1f);
+            
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(m_APlusB, m_A);
+            Gizmos.DrawSphere(m_APlusB, 0.1f);
+        }
+        
+        //------------------------------------------------------------------------------------------------------------------------------------
     }
 }
