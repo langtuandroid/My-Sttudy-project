1ddc82526ae619a42b4aa4e6209333e7512dee55
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-10-31 23:21:23 +0600

    Walk Gravity Added

--- Assets/Scripts/FK 3/Player/StateMachine/PlayerIdleState.cs
+++ Assets/Scripts/FK 3/Player/StateMachine/PlayerIdleState.cs
@@ -1,4 +1,6 @@
-﻿namespace FK_3.Player.StateMachine
+﻿using UnityEngine;
+
+namespace FK_3.Player.StateMachine
 {
     public class PlayerIdleState : PlayerBaseState
     {
@@ -12,6 +14,8 @@
 
             Ctx.ApplyMovementX = 0f;
             Ctx.ApplyMovementZ = 0f;
+
+            HandleGravity();
         }
 
         public override void UpdateState()
@@ -35,6 +39,33 @@
         public override void InitializeSubState()
         {
             
+        }
+        
+        private void HandleGravity()
+        {
+            if(Ctx.IsJumping) return;
+            
+            bool isFalling = Ctx.CurrentMovementY <= 0.0f || !Ctx.IsJumpPressed;
+            float fallMultiplier = 2.0f;
+            
+            if (Ctx.CharacterController.isGrounded)
+            {
+                Ctx.CurrentMovementY = Ctx.GroundedGravity;
+                Ctx.ApplyMovementY  = Ctx.GroundedGravity;
+            }
+            
+            else if (isFalling)
+            {
+                float previousYVelocity = Ctx.CurrentMovementY;
+                Ctx.CurrentMovementY += Ctx.Gravity * fallMultiplier * Time.deltaTime;
+                Ctx.ApplyMovementY = Mathf.Max((previousYVelocity + Ctx.CurrentMovementY) * 0.5f, -20.0f);
+            }
+            else
+            {
+                float previousYVelocity = Ctx.CurrentMovementY;
+                Ctx.CurrentMovementY += Ctx.Gravity * Time.deltaTime;
+                Ctx.ApplyMovementY = (previousYVelocity + Ctx.CurrentMovementY) * 0.5f;
+            }
         }
     }
 }
