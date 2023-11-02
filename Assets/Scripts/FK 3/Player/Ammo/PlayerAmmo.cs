c07d4e8ed155060dcb70c703ce07cfe6c17c67b3
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-11-03 01:25:29 +0600

    Projectile grave, reload and set Implemented

--- Assets/Scripts/FK 3/Player/Ammo/PlayerAmmo.cs
+++ Assets/Scripts/FK 3/Player/Ammo/PlayerAmmo.cs
@@ -7,6 +7,7 @@ namespace FK_3.Player.Ammo
         [SerializeField] private ProjectileTypeSo m_ProjectileTypeSo;
         [SerializeField] private Transform m_ProjectileGravingPoint;
         [SerializeField] private Transform m_ProjectileReloadPoint;
+        [SerializeField] private Transform m_ProjectileSetPoint;
         
         private GameObject projectile;
 
@@ -16,11 +17,18 @@ namespace FK_3.Player.Ammo
         {
             projectile = Instantiate(m_ProjectileTypeSo.m_Projectile, m_ProjectileGravingPoint.position, Quaternion.identity);
             projectile.transform.SetParent(m_ProjectileGravingPoint);
+            projectile.transform.position = m_ProjectileGravingPoint.position;
         }
         public void ReloadProjectile()
         {
-            projectile.transform.SetParent(null);
             projectile.transform.SetParent(m_ProjectileReloadPoint);
+            projectile.transform.position = m_ProjectileReloadPoint.position;
+        }
+        
+        public void SetProjectile()
+        {
+            projectile.transform.SetParent(m_ProjectileSetPoint);
+            projectile.transform.position = m_ProjectileSetPoint.position;
         }
         
         #endregion
