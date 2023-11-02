57e93e266b6f0c3260ad430a81fd0122a4037387
Anzamul Haque Akash <81710865+Anzamul-Haque-Akash@users.noreply.github.com>
2023-11-02 23:52:03 +0600

    Projectile Update

---
+++ Assets/Scripts/FK 3/Player/Ammo/PlayerAmmo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace FK_3.Player.Ammo
+{
+    public class PlayerAmmo : MonoBehaviour
+    {
+        [SerializeField] private ProjectileTypeSo m_ProjectileTypeSo;
+        [SerializeField] private Transform m_ProjectileGravingPoint;
+        [SerializeField] private Transform m_ProjectileReloadPoint;
+        
+        private GameObject projectile;
+
+        #region Animation Function
+        
+        public void SpawnProjectile()
+        {
+            projectile = Instantiate(m_ProjectileTypeSo.m_Projectile, m_ProjectileGravingPoint.position, Quaternion.identity);
+            projectile.transform.SetParent(m_ProjectileGravingPoint);
+        }
+        public void ReloadProjectile()
+        {
+            projectile.transform.SetParent(null);
+            projectile.transform.SetParent(m_ProjectileReloadPoint);
+        }
+        
+        #endregion
+    }
+}
---
+++ Assets/Scripts/FK 3/Player/Ammo/ProjectileTypeSo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace FK_3.Player.Ammo
+{
+    [CreateAssetMenu(fileName = "Player", menuName = "Player/ProjectileTypeSo")]
+    public class ProjectileTypeSo : ScriptableObject
+    {
+        public GameObject m_Projectile;
+    }
+}
